f6332390eb99281741b207c95a7d709982516dfd
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 21:59:01 +0900

    [feat]初期化処理

--- Warm-eatan/Assets/Miyamoto/Scripts/Question.cs
+++ Warm-eatan/Assets/Miyamoto/Scripts/Question.cs
@@ -1,6 +1,27 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class Question : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshPro _textMesh;
+    private QuestionSystem _questionSystem;
+
+    private void Awake()
+    {
+        _questionSystem = GetComponent<QuestionSystem>();
+        _textMesh.text = _questionSystem.Question.ToString();
+    }
+    private void OnEnable()
+    {
+       _questionSystem.OnReset += ResetQuestion;
+    }
+    private void OnDisable()
+    {
+        _questionSystem.OnReset += ResetQuestion;
+    }
+
+    private void ResetQuestion()
+    {
+        _textMesh.text = _questionSystem.Question.ToString();
+    }
 }
