98aa5c29c203d74da660a3fbdda5118abf4146a9
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-12-25 16:20:42 +0900

    [add]初期化処理作成

--- Warm-eatan/Assets/Miyamoto/Scripts/QuestionSystem.cs
+++ Warm-eatan/Assets/Miyamoto/Scripts/QuestionSystem.cs
@@ -2,5 +2,16 @@
 
 public class QuestionSystem : MonoBehaviour
 {
-    
+    public WordData Question => _question;
+
+    private WordData _question;
+    private WordManager _wordManager;
+    private void Awake()
+    {
+        _wordManager = FindAnyObjectByType<WordManager>();
+    }
+    private void Start()
+    {
+        _question = _wordManager.SetQuestion();
+    }
 }
